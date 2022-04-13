8a67f2475b781460f13f633a94dcdae77a7fc00c
Jaskarn <jaskarn15@gmail.com>
2022-04-13 22:29:12 +0000

    display message when connection is established

--- client.js
+++ client.js
@@ -9,14 +9,18 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("successfully connected to game server");
+  });
   conn.on("data", (data) => {
     console.log("server just sent you a message");
     console.log(data);
     console.log("it's got a sharp tongue this server");
   });
-
+ 
   return conn;
 };
 
 
 module.exports = connect;
+
