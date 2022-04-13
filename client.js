119e9249f16b57bb3de16ab26662b1edde03539c
Jaskarn <jaskarn15@gmail.com>
2022-04-13 23:16:20 +0000

    direction commands

--- client.js
+++ client.js
@@ -12,7 +12,32 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("successfully connected to game server");
     conn.write("Name: JAS");
+    //conn.write("Move: up");
   });
+  // conn.on("connect", () => {
+  //   conn.write("Move: up");
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: up"), 2000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: up"), 4000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: left"), 6000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: left"), 8000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: down"), 10000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: left"), 12000);
+  // });
+  // conn.on("connect", () => {
+  //   setTimeout(() => conn.write("Move: up"), 14000);
+  // });
   conn.on("data", (data) => {
     console.log("server just sent you a message");
     console.log(data);
