e29bbf7b4652b6d9a50a7518052a396a6b382551
Jaskarn <jaskarn15@gmail.com>
2022-04-13 22:08:08 +0000

    client module

---
+++ client.js
@@ -0,0 +1,22 @@
+//connect function
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("server just sent you a message");
+    console.log(data);
+    console.log("it's got a sharp tongue this server");
+  });
+
+  return conn;
+};
+
+
+module.exports = connect;
