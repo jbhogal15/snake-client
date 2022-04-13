8e9bed4fda2978ef55558a41f546c3ed943d622e
Jaskarn <jaskarn15@gmail.com>
2022-04-13 23:37:47 +0000

    input commands

--- play.js
+++ play.js
@@ -1,23 +1,57 @@
-const net = require("net");
+const connect = require('./client');
+
+
+
+
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541 // PORT number here,
-  });
+// const connect = function() {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",// IP address here,
+//     port: 50541 // PORT number here,
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log("server just sent you a message");
-    console.log(data);
-    console.log("it's got a sharp tongue this server");
-  });
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+//   conn.on("data", (data) => {
+//     console.log("server just sent you a message");
+//     console.log(data);
+//     console.log("it's got a sharp tongue this server");
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+// conn.on("connect", () => {
+//   conn.write("Move: up");
+// });
+// conn.on("connect", () => {
+//   setTimeout(() => conn.write("Move: right"), 2000);
+// });
+// conn.on("connect", () => {
+//   setTimeout(() => conn.write("Move: up"), 4000);
+// });
+// conn.on("connect", () => {
+//   setTimeout(() => conn.write("Move: left"), 6000);
+// });
 
