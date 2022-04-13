8e828927477f264116346e3f198a456950addc4f
Jaskarn <jaskarn15@gmail.com>
2022-04-13 22:34:58 +0000

    name command

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function() {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("successfully connected to game server");
+    conn.write("Name: JAS");
   });
   conn.on("data", (data) => {
     console.log("server just sent you a message");
