bc58a0f1902b225b9e5cf7179a8244a1026ca392
Jaskarn <jaskarn15@gmail.com>
2022-04-14 02:32:20 +0000

    movement commands

--- input.js
+++ input.js
@@ -1,22 +1,36 @@
+const connect = require('./client');
+let connection;
 
+const handleUserInput = function(key) {
+   
+  if (key === '\u0003') {
+    process.stdout.write('exited game\n');
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+};
 
-
-
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  stdin.on('data', handleUserInput);
   stdin.resume();
 
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
   return stdin;
 };
-
 
 
 module.exports = setupInput;
