5044cd5c0e7e6cd72146fcc9f72fa12b981bb270
Jaskarn <jaskarn15@gmail.com>
2022-04-14 03:12:29 +0000

    message command

--- input.js
+++ input.js
@@ -19,6 +19,9 @@ const handleUserInput = function(key) {
   if (key === "d") {
     connection.write("Move: right");
   }
+  if (key === "j") {
+    connection.write("Say: HOLA");
+  }
 };
 
 const setupInput = function(conn) {
